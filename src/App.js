d2a6109bdc3a112a93f614b75aa742c679eae74e
faldji <fadialamessi@gmail.com>
2019-09-19 10:54:00 +0200

    Intégration de l'API

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             users: [{id: 1, name: "Joueur1", main: ''}, {id: 2, name: "Joueur2", main: ''}],
             toggleUser: false,
             startGame: false,
-            masque: '',
+            masqueId: 0,
             display: '',
             ended: false,
             tryOut: 0,
@@ -26,84 +26,128 @@ class App extends Component {
         }
     }
 
-    // utilisation de la fonction flêchée pour gérer le binding this
-    handleClickBtn = (id) => {
-        const {letters, masque, ended, display, tryOut, users, toggleUser, winLose} = this.state;
-        const currentLetter = letters.find((lt) => lt.id === id);
-        let isEnded = ended;
-        const nTryout = tryOut + 1;
-        const currUser = toggleUser ? users[1] : users[0];
-        currUser.main = currentLetter.letter;
-        const newDisplay = masque.replace(/\w/g,
-            (letter, index) => (currentLetter.letter === letter.toUpperCase() && display.charAt(index) === '-' ? letter.toUpperCase() : display.charAt(index)));
-        isEnded = !newDisplay.includes('-');
-        const isWordOk = display !== newDisplay;
-        if (winLose === 1 && !isWordOk) {
-            isEnded = true;
-        }
-
-        this.setState(
-            prevState => {
-                return ({
-                    letters: prevState.letters.map(
-                        (item) => {
-                            if (item.id === currentLetter.id){
-                              item.disabled = true;
-                              if (isWordOk)
-                              item.validated = true;
-                            }
-                            return item;
-                        }
-                    ),
-                    users: prevState.users.map(
-                        (item) => {
-                            if (!isWordOk)
-                                item.main = 'Au tours du ' + item.name;
-                            else
-                                item.main = item.name + ' a trouvé ' + currentLetter.letter;
-                            return item;
-                        }
-                    ),
-                    startGame: !isEnded,
-                    display: newDisplay,
-                    ended: isEnded,
-                    tryOut: nTryout,
-                    toggleUser: prevState.winLose > 1 ? !prevState.toggleUser : prevState.toggleUser,
-                    winLose: !isWordOk && prevState.winLose > 0 ? prevState.winLose - 1 : prevState.winLose,
-                });
-            }
-        );
-    };
-    handleNewGame = () => {
-        const mot = this.generateNewMask();
-        const view = mot.replace(/\w/g, (letter) => (letter === ' ' ? ' ' : '-'));
-        this.setState(prevState => {
-            return ({
-                letters: prevState.letters.map(
-                    item => {
-                        if (item.disabled)
-                            item.disabled = false;
-                        if (item.validated)
-                          item.validated = false;
-                        return item;
-                    }),
-                users: prevState.users.map(
-                    (item) => {
-                        item.main = '';
-                        return item;
-                    }
-                ),
-                startGame: true, masque: mot, display: view, ended: false, tryOut: 0, toggleUser: false, winLose: 6
-            });
-        });
-    };
-
-    generateNewMask() {
-        const mots = ["test", "Le pendu", "reactjs", "jeux", "je me forme en react"];
-        const rndMotIdn = Math.floor(Math.random() * mots.length);
-        return mots[rndMotIdn];
+    /**
+     * Initialisation et récupération des données à partir de l'API
+     */
+    componentDidMount() {
     }
 
+    /**
+     * Fonction d'appel de l'API
+     * Utilisation de la fonction flêchée pour gérer le binding this
+     *
+     */
+    handleNewGame = () => {
+        const rndMotIndex = Math.floor((Math.random() * 5)+1);
+        const httpInput = "https://my-json-server.typicode.com/faldji/AsvamApiREST_PHP/mots/"+rndMotIndex;
+        fetch(httpInput)
+            .then((value) => value.json())
+            .then((value) => {
+                const view = value.masque.replace(/\w/g, (letter) => (letter === ' ' ? ' ' : '-'));
+                this.setState(prevState => {
+                    return ({
+                        letters: prevState.letters.map(
+                            item => {
+                                if (item.disabled)
+                                    item.disabled = false;
+                                if (item.validated)
+                                    item.validated = false;
+                                return item;
+                            }),
+                        users: prevState.users.map(
+                            (item) => {
+                                item.main = '';
+                                return item;
+                            }
+                        ),
+                        masqueId: rndMotIndex, display: view, startGame: true,  ended: false, tryOut: 0,
+                        toggleUser: false, winLose: 6,
+                    });
+                });
+            })
+            .catch(reason => console.error(reason));
+
+    };
+
+    /**
+     * Fonction de génération du nouveau masque
+     * @param id : int
+     * @param disp : string
+     * @param curLet : Alphabet
+     * Utilisation de la fonction flêchée pour gérer le binding this
+     *
+     */
+    generateNewDisplay(id,disp,curLet) {
+        const httpInput = "https://my-json-server.typicode.com/faldji/AsvamApiREST_PHP/mots/"+id;
+        fetch(httpInput)
+            .then((value) => value.json())
+            .then((value) => {
+                const {ended,display,tryOut, winLose} = this.state;
+                let isEnded = ended;
+                const nTryout = tryOut + 1;
+                const newDisplay = value.masque.replace(/\w/g,
+                (letter, index) =>
+                    (curLet.letter === letter.toUpperCase() &&
+                    disp.charAt(index) === '-' ? letter.toUpperCase() : disp.charAt(index)));
+                    isEnded = !newDisplay.includes('-');
+                    const isWordOk = display !== newDisplay;
+                    if (winLose === 1 && !isWordOk) {
+                        isEnded = true;
+                    }
+                    this.setState(
+                        prevState => {
+                            return ({
+                                letters: prevState.letters.map(
+                                    (item) => {
+                                        if (item.id === curLet.id){
+                                            item.disabled = true;
+                                            if (isWordOk)
+                                                item.validated = true;
+                                        }
+                                        return item;
+                                    }
+                                ),
+                                users: prevState.users.map(
+                                    (item) => {
+                                        if (!isWordOk)
+                                            item.main = 'Au tours du ' + item.name;
+                                        else
+                                            item.main = item.name + ' a trouvé ' + curLet.letter;
+                                        return item;
+                                    }
+                                ),
+                                startGame: !isEnded,
+                                ended: isEnded,
+                                tryOut: nTryout,
+                                display: newDisplay,
+                                toggleUser: prevState.winLose > 1 && !isWordOk  ? !prevState.toggleUser : prevState.toggleUser,
+                                winLose: !isWordOk && prevState.winLose > 0 ? prevState.winLose - 1 : prevState.winLose,
+                            });
+                        }
+                    );
+            }
+            )
+            .catch(reason => console.error(reason));
+    }
+
+    /**
+     * Fonction de gestion de l'évenement du click sur un boutton du keyboard
+     * @param id : int
+     * Utilisation de la fonction flêchée pour gérer le binding this
+     *
+     */
+    handleClickBtn = (id) => {
+        const {letters, masqueId,  display, users, toggleUser} = this.state;
+        const currentLetter = letters.find((lt) => lt.id === id);
+        const currUser = toggleUser ? users[1] : users[0];
+        currUser.main = currentLetter.letter;
+        this.generateNewDisplay(masqueId, display,currentLetter);
+    };
+
+    /**
+     * Render
+     * @returns {*}
+     */
     render() {
         const {letters, display, startGame, tryOut, users, toggleUser, winLose, ended} = this.state;
         const hangmans = [hangman0, hangman1, hangman2, hangman3, hangman4, hangman5, hangman6];
@@ -123,10 +167,10 @@ class App extends Component {
                     Essaie : {tryOut}
                 </div>
                 <div className="App">
-                    <img className="hangman" alt="hangman" src={hangman}/>
-                    <div className="masque-container">{display}</div>
+                   <img className="hangman" alt="hangman" src={hangman}/>
+                    {startGame && <div className="masque-container">{display}</div>}
                     <div className="Keyboard-container">
-                        {startGame ? keboardArray : btnNg}
+                        {startGame && !ended ? keboardArray : btnNg}
                     </div>
                     <div style={{display: newGameMask}} className="main-joueur">
                         <span> {lastUser.main}</span>
